ac94643bd1eddf054bdf5951e5ce9382bc030fb5
Justus Wingert <juw@solute.de>
2015-09-03 08:37:05 +0200

    Added another set of tests for CardinalLayout.

--- solute/epfl/test/components/test_cardinal_layout.py
+++ solute/epfl/test/components/test_cardinal_layout.py
@@ -8,6 +8,11 @@ def bool_toggle(request):
     return request.param
 
 
+@pytest.fixture(params=['center', 'west', 'east', 'north', 'south'])
+def cardinal(request):
+    return request.param
+
+
 def test_constrained(page, bool_toggle):
     page.root_node = components.CardinalLayout(
         constrained=bool_toggle,
@@ -30,3 +35,49 @@ def test_constrained(page, bool_toggle):
         assert out.startswith('<div class="container"')
     else:
         assert out.startswith('<div class="container-fluid"')
+
+
+def test_component_separation(page, cardinal):
+    default_compo = components.Text(value='some text')
+
+    page.root_node = components.CardinalLayout(
+        node_list=[
+            default_compo(slot=cardinal),
+            default_compo(slot=cardinal),
+            default_compo(slot=cardinal),
+            components.CardinalLayout(
+                slot=cardinal,
+                cid='fixed_layout',
+                node_list=[
+                    default_compo(cid='east', slot='east'),
+                    default_compo(cid='west', slot='west'),
+                    default_compo(cid='north', slot='north'),
+                    default_compo(cid='south', slot='south'),
+                    default_compo(cid='default'),
+                    default_compo(cid='center', slot='center'),
+                ]
+            )
+        ]
+    )
+
+    page.handle_transaction()
+    fixed_layout = page.fixed_layout
+
+    for c in ['center', 'east', 'west', 'north', 'south']:
+        if cardinal == c:
+            assert page.root_node.has_cardinal(c)
+        else:
+            assert not page.root_node.has_cardinal(c)
+
+    assert fixed_layout.has_cardinal('east')
+    assert fixed_layout.has_cardinal('west')
+    assert fixed_layout.has_cardinal('north')
+    assert fixed_layout.has_cardinal('south')
+    assert fixed_layout.has_cardinal('center')
+
+    assert ['east'] == [c.cid for c in fixed_layout.cardinal_components('east')]
+    assert ['west'] == [c.cid for c in fixed_layout.cardinal_components('west')]
+    assert ['north'] == [c.cid for c in fixed_layout.cardinal_components('north')]
+    assert ['south'] == [c.cid for c in fixed_layout.cardinal_components('south')]
+
+    assert ['default', 'center'] == [c.cid for c in fixed_layout.cardinal_components('center')]
