305d9e80f19b927e3a2908ce79ffbaa43391673a
Justus Wingert <juw@solute.de>
2015-09-22 09:26:33 +0200

    Switched to correct way to send events on Form.

--- solute/epfl/components/form/static/form.js
+++ solute/epfl/components/form/static/form.js
@@ -1,9 +1,13 @@
 epfl.Form = function (cid, params) {
     epfl.ComponentBase.call(this, cid, params);
-
-	$('#'+cid).submit(function(event) {
-	    event.preventDefault();
-	    epfl.dispatch_event(cid, "submit", {});
-	});
 };
 epfl.Form.inherits_from(epfl.ComponentBase);
+
+
+epfl.Form.prototype.after_response = function () {
+    var obj = this;
+	this.elm.submit(function(event) {
+	    event.preventDefault();
+	    obj.send_event("submit", {});
+	});
+};
