3215a17efecbe6f73382664712ee64877a8af031
Justus Wingert <juw@solute.de>
2015-09-22 09:36:48 +0200

    Switched to correct way to send events on Diagram.

--- solute/epfl/components/diagram/static/diagram.js
+++ solute/epfl/components/diagram/static/diagram.js
@@ -1,22 +1,26 @@
 epfl.Diagram = function (cid, params) {
     epfl.ComponentBase.call(this, cid, params);
-    $('#'+cid).highcharts(
-        params
-    );
-    // handle changes in series visibility
-    $('#'+cid).find('.highcharts-legend-item').click(function(event) {
-    	var my_chart = $('#'+cid).highcharts();
-    	series_visibility = [];
-        for (var i = 0; i < my_chart.series.length; i++) {
-		    series = my_chart.series[i];
-		    series_json = { "name": series.name };
-		    if (series.visible == false) {
-		    	series_json["visible"] = false;
-		    }
-		    series_visibility.push(series_json);
-		}
-		epfl.dispatch_event(cid, "visibilityChange", {"series_visibility":series_visibility});
-    });   
 };
-
 epfl.Diagram.inherits_from(epfl.ComponentBase);
+
+
+epfl.Diagram.prototype.after_response = function () {
+    var obj = this;
+
+    this.elm.highcharts(this.params);
+
+    // handle changes in series visibility
+    this.elm.find('.highcharts-legend-item').click(function (event) {
+        var my_chart = $('#' + cid).highcharts();
+        var series_visibility = [];
+        for (var i = 0; i < my_chart.series.length; i++) {
+            var series = my_chart.series[i];
+            var series_json = {"name": series.name};
+            if (series.visible == false) {
+                series_json["visible"] = false;
+            }
+            series_visibility.push(series_json);
+        }
+        obj.send_event("visibilityChange", {series_visibility: series_visibility});
+    });
+};
