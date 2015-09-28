d9ba468a49e2aebdf0e279d53c7b3f6b1f95d7ef
Justus Wingert <juw@solute.de>
2015-09-28 14:58:45 +0200

    Made event_name attribute compulsory for all children of LinkListLayout.

--- solute/epfl/components/link_list_layout/link_list_layout.py
+++ solute/epfl/components/link_list_layout/link_list_layout.py
@@ -71,7 +71,9 @@ class LinkListLayout(PaginatedListLayout):
 
         return links
 
-    @staticmethod
-    def default_child_cls(*args, **kwargs):
+    @classmethod
+    def default_child_cls(cls, *args, **kwargs):
         kwargs['list_element'] = True
+        if cls.event_name:
+            kwargs['event_name'] = cls.event_name
         return Link(*args, **kwargs)
