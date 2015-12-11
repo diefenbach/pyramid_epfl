8b25df3e1ead9916ec5dc39cd6dc95f40d42aff9
Jörg Kubaile <jku@solute.de>
2015-12-11 11:18:28 +0100

    added show_numbers to enable numbering instead of indicators

--- solute/epfl/components/carousel/static/carousel.js
+++ solute/epfl/components/carousel/static/carousel.js
@@ -7,5 +7,11 @@ epfl.Carousel.inherits_from(epfl.ComponentBase);
 epfl.Carousel.prototype.after_response = function (data) {
     epfl.ComponentBase.prototype.after_response.call(this, data);
     this.elm.carousel();
-};
 
+    var object = this.elm;
+    this.elm.on('slid.bs.carousel', function() {
+        var total_items = object.find('div.item').length;
+        var current_index = object.find('div.active').index() + 1;
+        object.find('div.carousel-numbers').html(current_index + ' / ' + total_items);
+    });
+};
