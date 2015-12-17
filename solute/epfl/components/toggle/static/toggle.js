9ce8c14412f0120d85e466fbc743276acbe329e6
Jörg Kubaile <jku@solute.de>
2015-12-17 10:08:58 +0100

    new style for Toggle

--- solute/epfl/components/toggle/static/toggle.js
+++ solute/epfl/components/toggle/static/toggle.js
@@ -1,16 +1,21 @@
 epfl.Toggle = function (cid, params) {
     epfl.ComponentBase.call(this, cid, params);
-
-    var selector = "#" + cid + "_input";
-    $(selector).attr('checked', $(selector).val() == 'True');
-    $(selector).bootstrapSwitch('state');
-    var compo = this;
-    var enqueue_event = !params["fire_change_immediately"];
-
-    $(selector).on('switchChange.bootstrapSwitch', function (event, state) {
-        var val = $(this).closest("div").parent().hasClass("bootstrap-switch-on");
-        epfl.FormInputBase.on_change(compo, val, cid, enqueue_event);
-    });
 };
 
 epfl.Toggle.inherits_from(epfl.ComponentBase);
+
+epfl.Toggle.prototype.after_response = function(data) {
+    epfl.ComponentBase.prototype.after_response.call(this, data);
+
+    var obj = this;
+    var enqueue_event = !this.params.fire_change_immediately;
+    var selector = "#" + this.cid + "_input";
+
+    $(selector).attr('checked', $(selector).val() == 'True');
+    $(selector).bootstrapSwitch('state');
+
+    $(selector).on('switchChange.bootstrapSwitch', function (event, state) {
+        var val = obj.elm.find('.bootstrap-switch').hasClass('bootstrap-switch-on');
+        epfl.FormInputBase.on_change(obj, val, obj.cid, enqueue_event);
+    });
+};
--- solute/epfl/components/toggle/toggle.py
+++ solute/epfl/components/toggle/toggle.py
@@ -4,30 +4,32 @@ from solute.epfl.components.form.inputbase import FormInputBase
 class Toggle(FormInputBase):
     """
     A form checkbox styled as toggle.
-    
+
     Typically, this component is used in a form:
-    
+
     .. code:: python
-        
+
         form = Form(node_list=[Toggle(label="Enable/Disable user:", name="user_enable_toggle")])
-    
+
     """
-    
+
     template_name = "toggle/toggle.html"
-    
+    js_parts = []
     js_name = FormInputBase.js_name + [("solute.epfl.components:toggle/static", "bootstrap-switch.min.js"),
                                        ("solute.epfl.components:toggle/static", "toggle.js")]
     css_name = FormInputBase.css_name + [("solute.epfl.components:toggle/static", "bootstrap-switch.min.css"),
                                          ("solute.epfl.components:toggle/static", "toggle.css")]
 
     validation_type = 'bool'  #: Validate this component as a boolean.
-    
+
     on_text = "on"  #: The text to be displayed if toggle is set to on.
     off_text = "off"  #: The text to be displayed if toggle is set to off.
     default = False  #: The default value of the toggle.
 
-    js_parts = FormInputBase.js_parts[:]
-    js_parts.extend(['toggle/toggle.js'])
+    new_style_compo = True
+    compo_js_params = ['fire_change_immediately']
+    compo_js_name = 'Toggle'
+    compo_js_extras = []
 
     def __init__(self, page, cid, on_text=None, off_text=None, default=None, **extra_params):
         """A toggling form input.
