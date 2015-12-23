96983bdeb8e261c7ebe91a64cc5ffb1541341577
Jörg Kubaile <jku@solute.de>
2015-12-23 08:08:13 +0100

    new style for hover link list layout.

--- solute/epfl/components/hover_link_list_layout/hover_link_list_layout.py
+++ solute/epfl/components/hover_link_list_layout/hover_link_list_layout.py
@@ -5,14 +5,24 @@
 """
 
 from solute.epfl.components import LinkListLayout
+from solute.epfl.components import PaginatedListLayout
 
 
 class HoverLinkListLayout(LinkListLayout):
+
+    theme_path = {'default': LinkListLayout.theme_path['default'],
+                  'row': ['hover_link_list_layout/theme']}
+
+    # LinkListLayout is not new style, so we go one more up
+    js_name = PaginatedListLayout.js_name + [('solute.epfl.components:hover_link_list_layout/static',
+                                              'hover_link_list_layout.js')]
+    new_style_compo = True
+    compo_js_name = 'HoverLinkListLayout'
+    compo_js_params = ['src']
+
     data_interface = {
         'id': None,
         'text': None,
         'src': None,
         'url': None
     }
-
-    js_parts = LinkListLayout.js_parts + ["hover_link_list_layout/hover_link_list_layout.js"]
