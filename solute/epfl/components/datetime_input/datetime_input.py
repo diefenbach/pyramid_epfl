6ae28aff712586dc319e327be956ec7ba7c0cf0a
Jörg Kubaile <jku@solute.de>
2016-01-04 08:53:10 +0100

    working on python code structure

--- solute/epfl/components/datetime_input/datetime_input.py
+++ solute/epfl/components/datetime_input/datetime_input.py
@@ -35,20 +35,20 @@ class DatetimeInput(FormInputBase):
                  name=None,
                  default=None,
                  label=None,
-                 mandatory=False,
-                 value=False,
-                 strip_value=False,
+                 mandatory=None,
+                 value=None,
+                 strip_value=None,
                  validation_error=None,
-                 fire_change_immediately=False,
+                 fire_change_immediately=None,
                  placeholder=None,
-                 readonly=False,
-                 submit_form_on_enter=False,
-                 input_focus=False,
+                 readonly=None,
+                 submit_form_on_enter=None,
+                 input_focus=None,
                  label_style=None,
                  input_style=None,
                  layout_vertical=None,
-                 compo_col=12,
-                 label_col=2,
+                 compo_col=None,
+                 label_col=None,
                  date_format=None,
                  calendar_icon=None,
                  **extra_params):
@@ -61,14 +61,16 @@ class DatetimeInput(FormInputBase):
         :param value: The actual value of the input element that is posted upon form submission
         :param strip_value: strip value if True in get value
         :param validation_error: Set during call of :func:`validate` with an error message if validation fails
-        :param fire_change_immediately: Set to true if input change events should be fired immediately to the server. Otherwise, change events are fired upon the next immediate epfl event
+        :param fire_change_immediately: Set to true if input change events should be fired immediately to the server.
+                                        Otherwise, change events are fired upon the next immediate epfl event
         :param placeholder: Placeholder text that can be displayed if supported by the input
         :param readonly: Set to true if input cannot be changed and is displayed in readonly mode
         :param submit_form_on_enter: If true, underlying form is submitted upon enter key in this input
         :param input_focus: Set focus on this input when component is displayed
         :param label_style: Can be used to add additional css styles for the label
         :param input_style: Can be used to add additional css styles for the input
-        :param layout_vertical: Set to true if label should be displayed on top of the input and not on the left before it
+        :param layout_vertical: Set to true if label should be displayed on top of the input and not on the left before
+                                it
         :param compo_col: Set the width of the complete input component (default: max: 12)
         :param label_col: Set the width of the complete input component (default: 2)
         :param validation_type: Set the validation type, default 'text'
--- solute/epfl/components/select/select.py
+++ solute/epfl/components/select/select.py
@@ -22,14 +22,54 @@ class Select(FormInputBase):
     compo_js_name = 'Select'
     compo_state = FormInputBase.compo_state + ['options']
 
-    options = None  #: A list if options for the select input.
+    # derived attribute overrides
     validation_type = 'text'  #: Validate input as text.
     layout_vertical = False  #: Use the vertical layout instead of the default horizontal.
 
-    def __init__(self, page, cid, options=None, layout_vertical=None, **extra_params):
+    # custom compo attributes
+    options = None  #: A list if options for the select input.
+
+    def __init__(self, page, cid,
+                 name=None,
+                 default=None,
+                 label=None,
+                 mandatory=None,
+                 value=None,
+                 strip_value=None,
+                 validation_error=None,
+                 fire_change_immediately=None,
+                 placeholder=None,
+                 readonly=None,
+                 submit_form_on_enter=None,
+                 input_focus=None,
+                 label_style=None,
+                 input_style=None,
+                 layout_vertical=None,
+                 compo_col=None,
+                 label_col=None,
+                 options=None,
+                 **extra_params):
         """Select form element that gives the selected option as its value.
 
+        :param name: An element without a name cannot have a value
+        :param default: Default value that may be pre-set or pre-selected
+        :param label: Optional label describing the input field
+        :param mandatory: Set to true if value has to be provided for this element in order to yield a valid form
+        :param value: The actual value of the input element that is posted upon form submission
+        :param strip_value: strip value if True in get value
+        :param validation_error: Set during call of :func:`validate` with an error message if validation fails
+        :param fire_change_immediately: Set to true if input change events should be fired immediately to the server.
+                                        Otherwise, change events are fired upon the next immediate epfl event
+        :param placeholder: Placeholder text that can be displayed if supported by the input
+        :param readonly: Set to true if input cannot be changed and is displayed in readonly mode
+        :param submit_form_on_enter: If true, underlying form is submitted upon enter key in this input
+        :param input_focus: Set focus on this input when component is displayed
+        :param label_style: Can be used to add additional css styles for the label
+        :param input_style: Can be used to add additional css styles for the input
+        :param layout_vertical: Set to true if label should be displayed on top of the input and not on the left before
+                                it
+        :param compo_col: Set the width of the complete input component (default: max: 12)
+        :param label_col: Set the width of the complete input component (default: 2)
         :param options: A list if options for the select input.
-        :param layout_vertical: Use the vertical layout instead of the default horizontal.
         """
         pass
