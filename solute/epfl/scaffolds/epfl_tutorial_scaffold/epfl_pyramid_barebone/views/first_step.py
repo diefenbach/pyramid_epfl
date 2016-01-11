c10f1dd9836ede8ef5b0cbfe450ce5bbfde1db5f
Jörg Kubaile <jku@solute.de>
2016-01-11 15:03:30 +0100

    finalized code for first step

--- solute/epfl/scaffolds/epfl_tutorial_scaffold/epfl_pyramid_barebone/views/first_step.py
+++ solute/epfl/scaffolds/epfl_tutorial_scaffold/epfl_pyramid_barebone/views/first_step.py
@@ -18,7 +18,9 @@ class NoteModel(ModelBase):
         self.data_store.setdefault('notes', []).append(note)
 
     def remove_note(self, note_id):
-        self.data_store['notes'] = [note for note in self.data_store['notes'] if note['id'] != note_id]
+        self.data_store['notes'] = [
+            note for note in self.data_store['notes'] if note['id'] != note_id
+        ]
 
     def get_note(self, note_id):
         return [note for note in self.data_store['notes'] if note['id'] == note_id][0]
@@ -35,22 +37,30 @@ class NoteForm(components.Form):
     id = None
     compo_state = components.Form.compo_state + ["id"]
 
-    node_list = [components.TextInput(label='Title',
-                                      name='title',
-                                      mandatory=True,
-                                      placeholder='Insert a title here!'),
-                 components.Textarea(label='Text',
-                                     mandatory=True,
-                                     name='text'),
-                 components.Button(value='Submit',
-                                   color='primary',
-                                   event_name='submit'),
-                 components.Button(value='Cancel',
-                                   event_name='cancel')]
+    node_list = [
+        components.TextInput(
+            label='Title',
+            name='title',
+            mandatory=True,
+            placeholder='Insert a title here!'),
+        components.Textarea(
+            label='Text',
+            mandatory=True,
+            name='text'),
+        components.Button(
+            value='Submit',
+            color='primary',
+            event_name='submit'),
+        components.Button(
+            value='Cancel',
+            event_name='cancel')
+    ]
 
     def handle_submit(self):
         if not self.validate():
-            self.page.show_fading_message('An error occurred in validating the form!', 'error')
+            self.page.show_fading_message(
+                'An error occurred in validating the form!', 'error'
+            )
             return
 
         note_value = self.get_values()
@@ -80,66 +90,111 @@ class NoteForm(components.Form):
         self.redraw()
 
 
-class NoteBox(components.Box):
-
-    is_removable = True
-    compo_state = components.Box.compo_state + ['text']
-
-    def init_struct(self):
-        self.node_list.append(components.Text(value=self.reflect.container_compo.text))
-        self.node_list.append(components.Button(value='Edit this note',
-                                                event_name='edit_note'))
-
-    def handle_edit_note(self):
-        self.page.note_form.load_note(self.id)
-
-    def handle_removed(self):
-        super(NoteBox, self).handle_removed()
-        if self.page.note_form.id == self.id:
-            self.page.note_form.clean_form()
-
-        self.page.model.remove_note(self.id)
-
-
 class NoteLayout(components.CardinalLayout):
 
-    node_list = [components.NavLayout(slot='north',
-                                      title='Epfl Tutorial App',
-                                      node_list=[components.Link(text='First step',
-                                                                 url='/',
-                                                                 slot='right'),
-                                                 components.Link(text='Second step',
-                                                                 url='/second',
-                                                                 slot='right'),
-                                                 components.Link(text='Third step',
-                                                                 url='/third',
-                                                                 slot='right')]
-                                      )]
+    plain = ['north']
     constrained = True
+    node_list = [
+        components.NavLayout(
+            slot='north',
+            title='Epfl Tutorial App',
+            node_list=[
+                components.Link(
+                    text='First step',
+                    url='/',
+                    slot='right'),
+                components.Link(
+                    text='Second step',
+                    url='/second',
+                    slot='right'),
+                components.Link(
+                    text='Third step',
+                    url='/third',
+                    slot='right')]
+        )]
 
 
 class FirstStepRoot(NoteLayout):
 
     def init_struct(self):
-        self.node_list.extend([components.Box(title='Edit note',
-                                              node_list=[NoteForm(cid='note_form')]),
-                               components.Box(cid="notes_list",
-                                              title='My notes',
-                                              default_child_cls=NoteBox,
-                                              data_interface={'id': None,
-                                                              'text': None,
-                                                              'title': None},
-                                              get_data='notes'),
-                               components.LinkListLayout(cid="notes_link_list",
-                                                         slot='west',
-                                                         auto_update_children=True,
-                                                         show_pagination=False,
-                                                         show_search=False,
-                                                         get_data='notes',
-                                                         data_interface={'id': None,
-                                                                         'url': 'note/{id}',
-                                                                         'text': 'title'})
-                               ])
+        self.node_list.extend([
+            components.Box(
+                title='Edit note',
+                node_list=[NoteForm(cid='note_form')]
+            ),
+            components.Box(
+                cid="notes_list",
+                title='My notes',
+                default_child_cls=components.Box(
+                    node_list=[
+                        components.Text(value=self.reflect.container_compo.text),
+                        components.ColLayout(
+                            node_list=[
+                                components.Button(
+                                    value='Edit this note',
+                                    color='primary',
+                                    cols=6,
+                                    event_name='edit_note'),
+                                components.Button(
+                                    value='Delete this note',
+                                    color='danger',
+                                    cols=6,
+                                    event_name='delete_note')]
+                        )]
+                ),
+                data_interface={
+                    'id': None,
+                    'text': None,
+                    'title': None},
+                get_data='notes'),
+            components.LinkListLayout(
+                cid="notes_link_list",
+                slot='west',
+                auto_update_children=True,
+                show_pagination=False,
+                show_search=False,
+                get_data='notes',
+                event_name='open_details',
+                data_interface={
+                    'id': None,
+                    'url': 'note/{id}',
+                    'text': 'title'}
+            )
+        ])
+
+    def handle_open_details(self):
+        calling_cid = self.epfl_event_trace[0]
+        note_id = self.page.components[calling_cid].id
+        note_data = self.page.model.get_note(note_id)
+
+        self.add_component(
+            components.ModalBox(
+                cid='note_detail_box',
+                title='Note Details',
+                node_list=[
+                    components.Text(
+                        tag='h3',
+                        verbose=True,
+                        value=note_data['title']),
+                    components.Text(
+                        value=note_data['text'])]
+                )
+            )
+        self.redraw()
+
+    def handle_edit_note(self):
+        calling_cid = self.epfl_event_trace[0]
+        note_id = self.page.components[calling_cid].container_compo.container_compo.id
+        self.page.note_form.load_note(note_id)
+
+    def handle_delete_note(self):
+        calling_cid = self.epfl_event_trace[0]
+        note_id = self.page.components[calling_cid].container_compo.container_compo.id
+
+        if self.page.note_form.id == note_id:
+            self.page.note_form.clean_form()
+
+        self.page.model.remove_note(note_id)
 
 
 @EPFLView(route_name='FirstStep', route_pattern='/')
