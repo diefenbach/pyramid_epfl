94ca61e0263902bc2ad701bccd43df4dd54d328a
Jörg Kubaile <jku@solute.de>
2016-02-05 08:50:01 +0100

    Added show_count and max_length to Textarea - and fixed initial len display of counter

--- solute/epfl/components/textarea/static/textarea.js
+++ solute/epfl/components/textarea/static/textarea.js
@@ -10,7 +10,19 @@ Object.defineProperty(epfl.Textarea.prototype, 'form_element', {
     }
 });
 
+epfl.Textarea.prototype.handle_keypress = function(event) {
+    if(this.params.max_length) {
+        $("#" + this.cid + '_input' + '_count').text(this.form_element.val().length);
+    }
+};
+
+
 epfl.Textarea.prototype.after_response = function(data) {
     epfl.FormInputBase.prototype.after_response.call(this, data);
     this.register_change_handler();
+
+    var compo = this;
+    if (compo.params.show_count) {
+        compo.elm.keyup(compo.handle_keypress.bind(compo));
+    }
 };
--- solute/epfl/test/components/test_textarea.py
+++ solute/epfl/test/components/test_textarea.py
@@ -1,3 +1,29 @@
 # * encoding: utf-8
 
-# Convenience Component, no tests required.
+import pytest
+from solute.epfl import components
+
+
+@pytest.fixture(params=[True, False])
+def bool_toggle(request):
+    return request.param
+
+
+def test_max_length(page, bool_toggle):
+    value = 'test value'
+    page.root_node = components.Textarea(
+        cid='text_with_title',
+        value=value,
+        max_length=120,
+        show_count=bool_toggle
+    )
+
+    page.handle_transaction()
+
+    assert 'maxlength="120"' in page.root_node.render()
+
+    assert_string = '_input_count">{len}</span>/120)</div>'.format(len=len(value))
+    if bool_toggle:
+        assert assert_string in page.root_node.render()
+    else:
+        assert assert_string not in page.root_node.render()
